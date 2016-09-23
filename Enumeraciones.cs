08893a3a5caf5858f2df64263e557ade58434247
chromerociencia <chromerociencia@outlook.com>
2016-09-23 16:47:51 -0300

    Se agrego tipo permiso

--- Enumeraciones.cs
+++ Enumeraciones.cs
@@ -31,3 +31,24 @@ public enum OrdenSel
     Ultima =2
 }
 
+public enum TipoPermiso
+{
+    Todo   =1,
+    Alta=2,
+    Baja=3,
+    Modificacion=4,
+    Ver = 5
+}
+
+
+public enum Formularios
+{
+    Paciente = 1,
+    Procedimiento =2,
+    DatosClinicos =3,
+    EstudiosPrevios=4,
+    Tavi =5,
+    Atc= 6,
+    Complicaciones=7,
+    _30Dias=8
+}
